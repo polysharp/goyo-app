072303a5db32446d56de4fc1727926af1d63d759
Henri Fardeau <henri.fardeau@outlook.fr>
2020-05-26 10:13:13 +0200

    fix: cookie domain

--- src/graphql/ApolloClient.js
+++ src/graphql/ApolloClient.js
@@ -3,13 +3,13 @@ import ApolloClient from 'apollo-boost';
 const apolloClient = new ApolloClient({
   uri:
     process.env.NODE_ENV === 'production'
-      ? 'https://api-goyo.netlify.app'
+      ? process.env.REACT_APP_API_PROD
       : process.env.REACT_APP_API_DEV,
   credentials: 'include',
   onError: ({ networkError }) => {
     if (networkError && networkError.statusCode === 401) {
-      // document.cookie = `${process.env.REACT_APP_AUTH_COOKIE}=;expires=Thu, 2 Aug 2001 20:47:11 UTC;`;
-      // window.location = '/';
+      document.cookie = `${process.env.REACT_APP_AUTH_COOKIE}=;expires=Thu, 2 Aug 2001 20:47:11 UTC;`;
+      window.location = '/';
     }
   },
 });

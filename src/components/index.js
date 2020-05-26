4816a1fc86b81812980e2329f799f06f14a6dcaf
Henri Fardeau <henri.fardeau@outlook.fr>
2020-05-26 10:17:49 +0200

    fix: cleaning unused components

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,5 @@
-import SignInForm from './Auth/SignInForm';
-import SignUpForm from './Auth/SignUpForm';
-
 import Layout from './Layout';
 import Menu from './Menu';
 import Board from './Board';
 
-export { SignInForm, SignUpForm, Layout, Menu, Board };
+export { Layout, Menu, Board };

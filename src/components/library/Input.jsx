839fbfc339738cd3c52127bdc0f9f8c165533adb
Henri Fardeau <henri.fardeau@outlook.fr>
2020-05-29 16:00:22 +0200

    fix: input background

--- src/components/library/Input.jsx
+++ src/components/library/Input.jsx
@@ -5,7 +5,7 @@ const Input = styled.input`
   ${tw`relative block w-full px-3 py-2 text-gray-900 bg-gray-100 border border-transparent rounded-md`}
 
   color: ${({ theme }) => theme['900']};
-  background-color: ${({ theme }) => theme['900']};
+  background-color: ${({ theme }) => theme['100']};
 
   &:focus {
     border-color: ${({ theme }) => theme['700']};

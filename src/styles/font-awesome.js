3c344be4c02a45219b8a9710683f8f66a6b43b84
Henri Fardeau <henri.fardeau@outlook.fr>
2020-05-31 16:43:50 +0200

    refactor: font-awesome library

---
+++ src/styles/font-awesome.js
@@ -0,0 +1,10 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faCogs,
+  faTimes,
+  faGlobe,
+  faYenSign,
+  faTheaterMasks,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add([faCogs, faTimes, faGlobe, faYenSign, faTheaterMasks]);

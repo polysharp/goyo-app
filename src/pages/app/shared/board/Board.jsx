f875f308c3a87221835d27aef6f3d9099bab88bd
Henri Fardeau <henri.fardeau@outlook.fr>
2020-06-01 18:39:44 +0200

    feat: board redesign

--- src/pages/app/shared/board/Board.jsx
+++ src/pages/app/shared/board/Board.jsx
@@ -3,8 +3,12 @@ import PropTypes from 'prop-types';
 
 const Board = ({ children }) => (
   <div
-    className="relative min-h-screen bg-white rounded-tl-lg rounded-bl-lg shadow-lg "
-    style={{ width: 'calc(100% - 70px)', marginLeft: '70px' }}
+    className="relative min-h-screen bg-gray-200 shadow-lg"
+    style={{
+      width: 'calc(100% - 70px)',
+      marginLeft: '70px',
+      borderTopLeftRadius: '1.5rem',
+    }}
   >
     {children}
   </div>
--- src/pages/app/shared/board/BordHeader.jsx
+++ src/pages/app/shared/board/BordHeader.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import tw from 'tailwind.macro';
 
 const BoardHeader = styled.header`
-  ${tw`flex items-center justify-between px-6 pt-12 pb-2 bg-gray-100 border rounded-tl-lg`}
+  ${tw`flex items-center justify-between px-6 pt-12 pb-2 rounded-tl-lg`}
 `;
 
 export default BoardHeader;

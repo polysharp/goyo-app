55c4918af87f9f8efbf26847ca24e55977ce90d8
Henri Fardeau <henri.fardeau@outlook.fr>
2020-06-02 13:30:35 +0200

    chore: extending tailwind

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      width: { '15': '3.75rem' },
+      height: { '15': '3.75rem' },
+      margin: { '15': '3.75rem' },
+      padding: { '15': '3.75rem' },
+    },
     container: {
       center: true,
     },

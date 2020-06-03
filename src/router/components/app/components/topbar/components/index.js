65303b7d87991621718f07077f5a88c51c737a8a
Henri Fardeau <henri.fardeau@outlook.fr>
2020-06-03 09:59:45 +0200

    feat: extracting home button

--- src/router/components/app/components/topbar/components/index.js
+++ src/router/components/app/components/topbar/components/index.js
@@ -1,5 +1,6 @@
+import Home from './Home';
 import Notifications from './Notifications';
 import Settings from './Settings';
 import Profile from './Profile';
 
-export { Notifications, Settings, Profile };
+export { Home, Notifications, Settings, Profile };
